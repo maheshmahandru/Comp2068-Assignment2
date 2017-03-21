979b4632a8db425317617493d9b1b0e46abc3926
maheshmahandru <mahesh.mahandru@gmail.com>
2017-03-21 00:38:22 -0400

    the lab has been connected with the database to the mlab

--- Assignment2/config/globals.js
+++ Assignment2/config/globals.js
@@ -1,3 +1,7 @@
 /**
  * Created by hpuser on 2017-03-21.
  */
+// array of global variables
+module.exports = {
+    db: 'mongodb://w2017dba:mahesh1997@ds137340.mlab.com:37340/comp2068-list'  // mlab
+};
